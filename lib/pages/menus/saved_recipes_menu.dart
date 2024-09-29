f832776de45a4b1111891b3860a04db1b4245df5
azmx <muhammadazzamaqiladafffa@gmail.com>
2024-09-29 16:16:52 +0700

    update saved recipe

--- lib/pages/menus/saved_recipes_menu.dart
+++ lib/pages/menus/saved_recipes_menu.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:food_application/pages/saved_menu_list.dart';
 import 'package:food_application/widget/card_image.dart';
+import 'package:food_application/widget/my_color.dart';
 
 class SavedRecipesMenu extends StatelessWidget {
   const SavedRecipesMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Container(
-        width: 600,
-        height: double.infinity,
-        child: SavedMenuList(),
-      )
-    ]);
+    return Scaffold(
+      appBar: AppBar(
+        surfaceTintColor: NatureColor.white,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: Text(
+          'Saved Recipes',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Stack(children: [
+          Container(
+            width: 600,
+            height: double.infinity,
+            child: SavedMenuList(),
+          )
+        ]),
+      ),
+    );
   }
 }
